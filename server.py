21698c7e5e84eda2f02e5906eebbd30818677749
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-06-19 01:56:28 -0400

    server.py --dry-run option.

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, request, render_template
 from wtforms import Form, StringField
 
@@ -16,12 +18,17 @@ def index():
     form = AnalyzeForm(request.form)
     results = {}
     error = None
-        try:
-            results = {'friends': analyze_friends(form.user_id),
-                       'followers': analyze_followers(form.user_id)}
-        except Exception as exc:
-            error = exc
     if request.method == 'POST' and form.validate() and form.user_id.data:
+        if app.config['DRY_RUN']:
+            time.sleep(2)
+            results = {'friends': (150, 25, 200),
+                       'followers': (200, 20, 250)}
+        else:
+            try:
+                results = {'friends': analyze_friends(form.user_id),
+                           'followers': analyze_followers(form.user_id)}
+            except Exception as exc:
+                error = exc
 
     return render_template('index.html',
                            form=form, results=results, error=error)
@@ -30,8 +37,10 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--dry-run', action='store_true')
     parser.add_argument('port', nargs=1, type=int)
     args = parser.parse_args()
     [port] = args.port
 
+    app.config['DRY_RUN'] = args.dry_run
     app.run(port=port, debug=args.debug)
