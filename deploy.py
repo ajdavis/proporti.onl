fe0c4a31f7b391c4da79fd7d12da244cbf60cbef
A. Jesse Jiryu Davis <jesse@mongodb.com>
2019-04-23 19:12:42 -0400

    use API to reload site after deploy
    Avoids having to open a browser and require the user to click "Reload".

---
+++ deploy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import argparse
+import os
+
+import requests
+
+my_domain = 'www.proporti.onl'
+username = 'emptysquare'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('token', metavar='PYTHON_ANYWHERE_TOKEN',
+                    help='A Python Anywhere API token for your account')
+
+args = parser.parse_args()
+
+print("Rsync files....")
+os.system(
+    "rsync -rv --exclude '*.pyc' *"
+    " emptysquare@ssh.pythonanywhere.com:www.proporti.onl/")
+
+print("Restarting....")
+uri = 'https://www.pythonanywhere.com/api/v0/user/{uname}/webapps/{dom}/reload/'
+response = requests.post(
+    uri.format(uname=username, dom=my_domain),
+    headers={'Authorization': 'Token {token}'.format(token=args.token)})
+
+if response.status_code == 200:
+    print('All OK')
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code,
+                                                       response.content))
